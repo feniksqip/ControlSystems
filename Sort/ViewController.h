465ad8380b774b44783606518056aa65ba674a40
Andrey <andrey@mac-admin.local>
2016-03-01 11:03:34 +0300

    Job one and two is done.

---
+++ Sort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sort
+//
+//  Created by Andrey on 26.02.16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sort/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  Sort
+//
+//  Created by Andrey on 26.02.16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    /*
+    Дан массив размера N из целых чисел, принимающих любое целое
+    значение в диапазоне от 1 до M, при этом одно число в этом массиве
+    повторяется два раза. Найти это число за время O(N).
+    */
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
